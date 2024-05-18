ee134d2dd8cfca89f02b34660fb25cc61fd04448
2021-sydney-tech <88696282+2021-sydney-tech@users.noreply.github.com>
2024-05-18 13:09:27 +0000

    correction the code "excludeSwitches"

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from selenium import webdriver
 def get_driver():
   # Set options to make browsing easier
   options = webdriver.ChromeOptions()
-  options.add_argument('disable-infobars') # disables the infobars
-  options.add_argument('start-maximized') # open Browser in maximized mode
-  options.add_argument('disable-dev-shm-usage') # disables the use of /dev/shm
-  options.add_argument('no-sandbox') # disables the sandbox
-  options.add_experimental_option('exclueSwitches', ['enable-automation']) # disables the automation
-  options.add_argument('disable-blink-features=AutomationControlled') # disables the automation
+  options.add_argument("disable-infobars") # disables the infobars
+  options.add_argument("start-maximized") # open Browser in maximized mode
+  options.add_argument("disable-dev-shm-usage") # disables the use of /dev/shm
+  options.add_argument("no-sandbox") # disables the sandbox
+  options.add_experimental_option("excludeSwitches", ["enable-automation"]) # disables the automation
+  options.add_argument("disable-blink-features=AutomationControlled") # disables the automation
   
   driver = webdriver.Chrome(options=options)
   driver.get("https://automated.pythonanywhere.com/")
@@ -16,7 +16,7 @@ def get_driver():
 
 def main():
   driver = get_driver()
-  element = driver.find_element(by='xpath', value='/html/body/div[1]/div/h1[1]')
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
   return element.text
 
 print(main())
